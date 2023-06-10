0f62a88c3291223ec393cdcf9da601e8762efde9
juneyoung-jo@github.com <juneyoung-jo@github.com>
2023-06-11 00:26:10 +0900

    init project

---
+++ src/main/kotlin/young/myboxhexagonal/MyBoxHexagonalApplication.kt
@@ -0,0 +1,11 @@
+package young.myboxhexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyBoxHexagonalApplication
+
+fun main(args: Array<String>) {
+    runApplication<MyBoxHexagonalApplication>(*args)
+}
---
+++ src/test/kotlin/young/myboxhexagonal/MyBoxHexagonalApplicationTests.kt
@@ -0,0 +1,13 @@
+package young.myboxhexagonal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyBoxHexagonalApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
